6d2af81042cd2874943553f9d3130f4921f1d62d
Arkuzeron <eduardomg@gid.uff.br>
2019-11-06 17:58:03 -0300

    parcial

--- lib/conecta_address_br.rb
+++ lib/conecta_address_br.rb
@@ -1,44 +1,28 @@
 require "conecta_address_br"
 
 module ConectaAddressBr
-  class Error < StandardError; end
-  
-  class State
-    def all
-      [
-        %w[Acre AC],
-        %w[Alagoas AL],
-        %w[Amapá AP],
-        %w[Amazonas AM],
-        %w[Bahia BA],
-        %w[Ceará CE],
-        ['Distrito Federal', 'DF'],
-        ['Espírito Santo', 'ES'],
-        %w[Goiás GO],
-        %w[Maranhão MA],
-        ['Mato Grosso', 'MT'],
-        ['Mato Grosso do Sul', 'MS'],
-        ['Minas Gerais', 'MG'],
-        %w[Pará PA],
-        %w[Paraíba PB],
-        %w[Paraná PR],
-        %w[Pernambuco PE],
-        %w[Piauí PI],
-        ['Rio de Janeiro', 'RJ'],
-        ['Rio Grande do Norte', 'RN'],
-        ['Rio Grande do Sul', 'RS'],
-        %w[Rondônia RO],
-        %w[Roraima RR],
-        ['Santa Catarina', 'SC'],
-        ['São Paulo', 'SP'],
-        %w[Sergipe SE],
-        %w[Tocantins TO]
-      ] 
-    end
+  def regexify(reg)
+    reg = reg.source if reg.respond_to?(:source) # Handle either a Regexp or a String that looks like a Regexp
+    reg
+      .gsub(%r{^\/?\^?}, '').gsub(%r{\$?\/?$}, '') # Ditch the anchors
+      .gsub(/\{(\d+)\}/, '{\1,\1}').gsub(/\?/, '{0,1}') # All {2} become {2,2} and ? become {0,1}
+      .gsub(/(\[[^\]]+\])\{(\d+),(\d+)\}/) { |_match| Regexp.last_match(1) * sample(Array(Range.new(Regexp.last_match(2).to_i, Regexp.last_match(3).to_i))) }                # [12]{1,2} becomes [12] or [12][12]
+      .gsub(/(\([^\)]+\))\{(\d+),(\d+)\}/) { |_match| Regexp.last_match(1) * sample(Array(Range.new(Regexp.last_match(2).to_i, Regexp.last_match(3).to_i))) }                # (12|34){1,2} becomes (12|34) or (12|34)(12|34)
+      .gsub(/(\\?.)\{(\d+),(\d+)\}/) { |_match| Regexp.last_match(1) * sample(Array(Range.new(Regexp.last_match(2).to_i, Regexp.last_match(3).to_i))) }                      # A{1,2} becomes A or AA or \d{3} becomes \d\d\d
+      .gsub(/\((.*?)\)/) { |match| sample(match.gsub(/[\(\)]/, '').split('|')) } # (this|that) becomes 'this' or 'that'
+      .gsub(/\[([^\]]+)\]/) { |match| match.gsub(/(\w\-\w)/) { |range| sample(Array(Range.new(*range.split('-')))) } } # All A-Z inside of [] become C (or X, or whatever)
+      .gsub(/\[([^\]]+)\]/) { |_match| sample(Regexp.last_match(1).split('')) } # All [ABC] become B (or A or C)
+      .gsub('\d') { |_match| sample(Numbers) }
+      .gsub('\w') { |_match| sample(Letters) }
   end
 
-  class City
-    def
+  def fetch(keys)
+    fetched = translate("faker.#{key}")
+    fetched = fetched.last if fetched.size <= 1
+    if !fetched.respond_to?(:sample) && fetched.match(%r{^\/}) && fetched.match(%r{\/$}) # A regex
+      regexify(fetched)
+    else
+      fetched
     end
   end
 end
---
+++ lib/conecta_address_br/states/states.rb
@@ -0,0 +1,7 @@
+module ConectaAddressBr
+    class State
+        def names
+            fetch('creature.animal.name')
+        end
+    end
+end
